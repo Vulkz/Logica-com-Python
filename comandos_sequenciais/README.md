25d99756a5e1e4ac6c14c6059cb7a1ded32595fb
Vulkz <gabrielcosta3536@gmail.com>
2022-09-11 18:51:28 -0300

    add question 1 with answer

---
+++ comandos_sequenciais/README.md
@@ -0,0 +1,5 @@
+# Comandos Sequenciais
+
+1. Desenvolva um programa em python que leia o nome de um aluno e suas duas notas semestrais. No final mostre o nome do aluno e a sua média.
+
+    *[*Resposta.*](exercicio_1.py)
---
+++ comandos_sequenciais/exercicio_1.py
@@ -0,0 +1,9 @@
+nome = input("Digite o nome do(a) aluno(a): ")
+
+n1 = float(input("Digite a 1° nota: "))
+n2 = float(input("Digite a 2° nota: "))
+
+media = (n1+n2)/2
+
+print(f"Nome: {nome}")
+print(f"Média: {media:.2f}")
